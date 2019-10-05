86064ca0b7c82bc9a34d933eee154b79d6bf90c1
Nick Lunn <nrlunn325@gmail.com>
2019-10-05 13:05:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 function scuberGreetingForFeet(ride){
   if (ride <= 400){
-    return 'This one is on me!'
+    return 'This one is on me!';
   } else if (ride > 2500){
-    return 'No can do.'
+    return 'No can do.';
   } else if (ride > 2000){
-    return 'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.';
   }
 }
 
 function ternaryCheckCity(city){
-  city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 function switchOnCharmFromTip(tip){
-  if (tip === 'generous'){
-    return 'Thank you so much.'
-  } else if (tip === 'not as generous') {
-    return 'Thank you.'
-  } else {
-    return 'Bye.'
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+      
   }
 }
