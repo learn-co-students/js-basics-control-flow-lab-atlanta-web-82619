e143c3c5ad74272bba36887a9ec3b8971c8fcea5
github username <github email address>
2019-10-06 19:55:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  let msg;
+  if (number < 400) {
+    msg = 'This one is on me!';
+  }
+  else if (number > 2500) {
+    msg = 'No can do.';
+  }
+  else if (number > 2000) {
+    msg = 'I will gladly take your thirty bucks.';
+  }
+  return msg;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let msg;
+
+  msg = (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+  return msg;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let msg;
+  switch (tip) {
+    case ('generous'):
+      msg = 'Thank you so much.';
+      break;
+    case ('not as generous'):
+      msg = 'Thank you.';
+      break;
+    default:
+      msg = 'Bye.';
+      break;
+  }
+  return msg;
 }
